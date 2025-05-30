82e213fc404c87f43fbbad61f87904d32c41f1f7
santilordi <santilordi569@gmail.com>
2025-05-30 10:01:07 -0300

    Inicio

---
+++ TechnoLife/src/main/java/com/Backend/TechnoLife/TechnoLifeApplication.java
@@ -0,0 +1,13 @@
+package com.Backend.TechnoLife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnoLifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnoLifeApplication.class, args);
+	}
+
+}
---
+++ TechnoLife/src/test/java/com/Backend/TechnoLife/TechnoLifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Backend.TechnoLife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnoLifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
