a9c951d8531c9185cc55f927d32395474e9bd432
santilordi <santilordi569@gmail.com>
2025-06-02 03:43:43 -0300

    fix controller

--- TechnoLife/src/main/java/com/Backend/TechnoLife/Repositories/ClientRepository.java
+++ TechnoLife/src/main/java/com/Backend/TechnoLife/Repositories/ClientRepository.java
@@ -4,6 +4,13 @@ import com.Backend.TechnoLife.Model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ClientRepository  extends JpaRepository<Client, Long> {
+    @Repository
+    public interface ClienteRepository extends JpaRepository<Client, Long> {
+        Optional<Client> findByEmail(String email);
+    }
+
 }
