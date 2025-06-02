03e4cdfe3ef81ddd19b8c3975f23c99160823da0
santilordi <santilordi569@gmail.com>
2025-06-02 04:19:25 -0300

    fix config

--- TechnoLife/src/main/java/com/Backend/TechnoLife/Config/WebConfig.java
+++ TechnoLife/src/main/java/com/Backend/TechnoLife/Config/WebConfig.java
@@ -12,7 +12,10 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:5500");
+                registry.addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5500", "http://127.0.0.1:8080", "null")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
             }
         };
     }
